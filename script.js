981af21b545f76ffc1bb66839f5ce9a30fa117bf
Lekner Norbert <leknern@kkszki.hu>
2022-09-08 19:10:31 +0200

    new JS

---
+++ script.js
@@ -0,0 +1,25 @@
+let a = 2;
+let b = 7;
+
+switch(a) {
+    case 1: {
+        console.log(a + 1);
+
+        break;
+    }
+    case 2: {
+        console.log(a + 2);
+
+        break;
+    }
+    case 3: {
+        console.log(a + 3);
+
+        break;
+    }
+    default: {
+        console.log(a);
+
+        break;
+    } 
+}
